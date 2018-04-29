0769cb83050707bd4df0414b2b3afe16273fd634
Gerald Degeneve <gerald.degeneve@gmail.com>
2018-04-29 13:48:01 +0200

    Add Deployment scripts

---
+++ deploy/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = "awake"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = "$(Join-Path $toolsDir $packageName).exe"
+
+$fileLocation
+$toolsDir
+$packageName
+
+Get-Process | Where-Object {$_.Name -like $packageName} | Stop-Process -Force
+Start-Process $fileLocation
+
+
---
+++ deploy/tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = "awake"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = "$(Join-Path $toolsDir $packageName).exe"
+
+Get-Process | Where-Object {$_.Path -like $fileLocation} | Stop-Process -Force
